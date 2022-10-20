ce7aecb146f5e1560c13d4ef480f7234106af6a9
Santiago Garcia Santos <santiago@garciasantos.org>
2022-10-19 23:33:31 -0400

    Absolutely crushed the home page
    We're so good

--- client/src/Home/HomePage.tsx
+++ client/src/Home/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 import { Typography, Grid } from '@mui/material';
+import Box from '@mui/system/Box';
 import { useAppDispatch, useAppSelector } from '../util/redux/hooks';
 import {
   logout as logoutAction,
@@ -11,6 +12,8 @@ import {
 import { logout as logoutApi, selfUpgrade } from './api';
 import ScreenGrid from '../components/ScreenGrid';
 import PrimaryButton from '../components/buttons/PrimaryButton';
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
 
 interface PromoteButtonProps {
   admin: boolean | null;
@@ -72,16 +75,57 @@ function HomePage() {
   const message = `Welcome to the Boilerplate, ${user.firstName} ${user.lastName}!`;
   return (
     <ScreenGrid>
-      <Typography variant="h2">{message}</Typography>
-      <Grid item container justifyContent="center">
-        <PromoteButton
-          admin={admin}
-          handleSelfPromote={handleSelfPromote}
-          navigator={navigator}
-        />
-      </Grid>
-      <Grid item container justifyContent="center">
-        <Button onClick={handleLogout}>Logout</Button>
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+        height="100%"
+        fit-content="100%"
+      >
+        <Grid item width="100%">
+          <NavBar />
+        </Grid>
+
+        <Grid
+          container
+          height="100%"
+          direction="column"
+          xs
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Grid container direction="column" alignItems="center" padding={2}>
+            <Typography variant="h2" fontWeight="bold" textAlign="center">
+              Abuse and Sexual Assault Resource Tree
+            </Typography>
+
+            <Grid item style={{ maxWidth: '600px' }}>
+              <Typography textAlign="center">
+                Answer a few questions to find the resources you need. If you
+                need immediate help, call Penn Police.
+              </Typography>
+            </Grid>
+
+            <Grid container direction="column" alignItems="center" padding={8}>
+              <Button variant="contained" size="large">
+                Begin
+              </Button>
+              <Box padding={2}>
+                <Button>Or, See all resources instead</Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container direction="column" padding={2} alignItems="center">
+          <Typography variant="body2" align="center" color="grayText">
+            Disclaimer: Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Typography>
+        </Grid>
+        <Grid item width="100%">
+          <Footer />
+        </Grid>
       </Grid>
     </ScreenGrid>
   );
